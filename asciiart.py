d3707754069c0660ab59940f4843629b568865db
Tathagata Roy <tathagata.roy1278@gmail.com>
2020-12-05 20:23:00 +0530

    Added Documentation

--- asciiart.py
+++ asciiart.py
@@ -9,27 +9,24 @@ grayImage = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
 tmp = grayImage
 grayImage = cv2.threshold(grayImage,ii,255,cv2.THRESH_BINARY)
 grayImage = grayImage[1]
-cv2.imshow("k",grayImage)
 
-cv2.waitKey(0)
 ch = ["*","#","0"]
 img = grayImage
 
 # resize image
-tmp = cv2.resize(tmp, (200,180), interpolation = cv2.INTER_AREA)
+tmp = cv2.resize(tmp, (125,50), interpolation = cv2.INTER_AREA)
 
 o = open("art.txt","w+")
 for i in range(0,len(tmp)):
 	for j in range(0,len(tmp[0])):
 		if(tmp[i][j]<=ii):
-			o.write("#")
+			print('#',end = '')
 		elif tmp[i][j]<=ii+d:
-			o.write("0")
+			print('0',end = '')
 		elif tmp[i][j]<=ii+2*d:
-			o.write("~")
+			print('~',end = '')
 		elif tmp[i][j]<=ii+3*d:
-			o.write("*")
+			print('*',end = '')
 		else:
-			o.write(" ")
-	
-	o.write("\n")
+			print(' ',end = '')
+	print()
