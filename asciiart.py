5422822554980828ec6f4abb4597393fcc78f7c2
Tathagata Roy <tathagata.roy1278@gmail.com>
2020-12-05 20:02:39 +0530

    Added Documentation

--- asciiart.py
+++ asciiart.py
@@ -1,8 +1,10 @@
 import cv2
 import random
 a = cv2.imread("test.jpg");
-ii = int(input())
-d = int(input())
+ii = int(input())# this defines the minimum threshold of colour after which the desired picture is obtained, which varies from person to person, so tweak it to find
+#optimum configuration, generally ranges from [0,150]
+d = int(input())#This defines the amount of variance of colour in the picture, A picture with uniform lighting has a low value, while a picture with high contrast value,
+#has high value, generally ranges from [20,80]
 grayImage = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
 tmp = grayImage
 grayImage = cv2.threshold(grayImage,ii,255,cv2.THRESH_BINARY)
